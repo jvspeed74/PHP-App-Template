3db9772161402f3a9448bed05cc8e2bf20b40ba4
jvspeed74 <javaugh@iu.edu>
2024-10-08 14:23:24 -0400

    Init

---
+++ src/RandomStringGenerator.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Techtrio\SlimF1Engine;
+
+use InvalidArgumentException;
+
+class RandomStringGenerator
+{
+    /**
+     * Generate a random string of the specified length.
+     *
+     * @param int $length
+     * @return string
+     */
+    public function generate(int $length = 10): string
+    {
+        if ($length < 1) {
+            throw new InvalidArgumentException("Length must be a positive integer.");
+        }
+
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+}
---
+++ tests/RandomStringGeneratorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+
+use PHPUnit\Framework\TestCase;
+use Techtrio\SlimF1Engine\RandomStringGenerator;
+
+class RandomStringGeneratorTest extends TestCase
+{
+    protected RandomStringGenerator $generator;
+
+    protected function setUp(): void
+    {
+        $this->generator = new RandomStringGenerator();
+    }
+
+    public function testGenerateReturnsStringOfGivenLength(): void
+    {
+        $length = 12;
+        $result = $this->generator->generate($length);
+        $this->assertIsString($result);
+        $this->assertEquals($length, strlen($result));
+    }
+
+    public function testGenerateThrowsExceptionForNegativeLength(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->generator->generate(-5);
+    }
+
+    public function testGenerateReturnsRandomString(): void
+    {
+        $result1 = $this->generator->generate(10);
+        $result2 = $this->generator->generate(10);
+
+        $this->assertNotEquals($result1, $result2, "Two generated strings should not be the same.");
+    }
+
+    public function testGenerateDefaultLength(): void
+    {
+        $result = $this->generator->generate();
+        $this->assertEquals(10, strlen($result), "Default string length should be 10.");
+    }
+}
